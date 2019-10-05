40477e8ea2b391dc2282fde05a86d8dd248dd5cb
James  Wallace <jameswallace@Jamess-MBP.lan1>
2019-10-04 23:37:33 -0400

    successfully implemented breadth first search to find a solution to the puzzle

--- eight_puzzle.py
+++ eight_puzzle.py
@@ -2,7 +2,7 @@ from collections import deque
 from numpy import random
 
 # board state represented as a list
-board_state = [3, 1, 2, 4, 0, 5, 6, 7, 8, -1]
+board_state = [0, 3, 2, 4, 1, 5, 8, 6, 7, -1]
 
 # dictionary to represent coordinate positions of the index of the board state array
 index_to_coordinates = {
@@ -14,7 +14,7 @@ index_to_coordinates = {
     5: (1, 2),
     6: (2, 0),
     7: (2, 1),
-    8: (2, 2)
+    8: (2, 2),
 }
 
 # dictionary to represent index of the board state array by the coordinates of the terminal board abstraction
@@ -43,28 +43,27 @@ coordinates_to_index = {
 }
 
 
-
 # print board
-def print_board(board_state):
+def print_board(state):
     for i in range(3):
         for j in range(3):
             print('|', end=' ')
 
-            if board_state[j + 3 * i] == 0:
+            if state[j + 3 * i] == 0:
                 print(' ', end=' ')
             else:
-                print(board_state[j + 3 * i], end=' ')
+                print(state[j + 3 * i], end=' ')
 
             print('|', end=' ')
         print()
 
 
 # count the number of inversions
-def count_inversions(board_state):
+def count_inversions(state):
     tiles = []
-    for i in range(len(board_state)):
-        if board_state[i] != 0:
-            tiles.append(board_state[i])
+    for i in range(len(state)):
+        if state[i] != 0 and state[i] != -1:
+            tiles.append(state[i])
 
     inversions = 0
     for i in range(len(tiles)):
@@ -76,14 +75,14 @@ def count_inversions(board_state):
 
 
 # randomize the board ensuring the new random state is solvable
-def randomize_board(board_state):
+def randomize_board(state):
     while True:
         tiles = random.permutation(9)
 
-        for i in range(len(board_state)):
+        for i in range(len(state)):
             board_state[i] = tiles[i]
 
-        if count_inversions(board_state) % 2 == 0:
+        if count_inversions(state) % 2 == 0:
             break
 
 
@@ -91,12 +90,6 @@ def randomize_board(board_state):
 def move_index(state, empty_index, index):
     state[empty_index] = state[index]
     state[index] = 0
-
-
-# copy board state to another state
-def copy_states(state, copy_state):
-    for i in range(len(state)):
-        copy_state.append(state[i])
 
 
 # move a tile into a coordinate position
@@ -123,35 +116,83 @@ def move_tile(state, tile):
         print('illegal move')
 
 
-
 # check to see if a tile is adjacent to the empty tile
 def adjacent_to_empty(state, tile):
     tile_index = state.index(tile)
 
-    row, column = index_to_coordinates.get(tile_index)
+    if tile_index != 9:
+        row, column = index_to_coordinates.get(tile_index)
 
-    if state[coordinates_to_index.get((row + 1, column))] == 0:
-        return 'above'
-    elif state[coordinates_to_index.get((row - 1, column))] == 0:
-        return 'below'
-    elif state[coordinates_to_index.get((row, column + 1))] == 0:
-        return 'left'
-    elif state[coordinates_to_index.get((row, column - 1))] == 0:
-        return 'right'
-    else:
-        return 'not'
+        if state[coordinates_to_index.get((row + 1, column))] == 0:
+            return 'above'
+        elif state[coordinates_to_index.get((row - 1, column))] == 0:
+            return 'below'
+        elif state[coordinates_to_index.get((row, column + 1))] == 0:
+            return 'left'
+        elif state[coordinates_to_index.get((row, column - 1))] == 0:
+            return 'right'
+        else:
+            return 'not'
+
+
+# return all the tiles adjacent to the empty tile
+def all_adjacent_to_empty(state):
+    adjacent = []
+    for i in range(len(state)):
+        if adjacent_to_empty(state, state[i]) is not 'not' and state[i] != -1:
+            adjacent.append(state[i])
+
+    return adjacent
+
+
+# get the successors of a state
+def get_successors(state):
+    adjacents = all_adjacent_to_empty(state)
+
+    successors = []
+    for tile in adjacents:
+        state_copy = state.copy()
+        move_tile(state_copy, tile)
+
+        successors.append(state_copy)
+
+    return successors
+
+
+# bfs search to find the next best move
+def bfs(state):
+    fringe = deque()
+    visited = set()
+
+    fringe.append(state)
+    while len(fringe) != 0:
+        s = fringe.popleft()
+        visited.add(tuple(s))
+
+        print_board(s)
+        print()
+
+        if count_inversions(s) == 0:
+            print('we found a goal state!!!!')
+            break
+
+        for successor in get_successors(s):
+            if tuple(successor) not in visited:
+                fringe.append(successor)
+
+    return 1
+
 
 # main
 def main():
-    while True:
-        print_board(board_state)
+    print_board(board_state)
 
-        if count_inversions(board_state) == 0:
-            print('you won!')
-            break
 
-        tile = input('move a tile: ')
-        move_tile(board_state, int(tile))
+    bfs(board_state)
+
+    tile = input('move a tile: ')
+    move_tile(board_state, int(tile))
+
 
 if __name__ == "__main__":
     main()
